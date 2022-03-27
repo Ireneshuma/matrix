14c9d6004d1f749ea874599f9d4612edabad9ee5
ishuma <Matesha31@gmail.com>
2022-03-27 23:52:44 +0200

    initial commit

---
+++ src/matrixx.java
@@ -0,0 +1,22 @@
+public class matrixx {
+    public static void main(String[] args) {
+
+        int[][] matrixx = new int[10][10];
+        int i, j;
+
+        for ( i = 0; i < 10; i++) {
+            for ( j=0; j<10;j++){
+                if (i==j)
+                    matrixx[i][j]=i;
+                else matrixx[i][j]=0;
+                System.out.println(matrixx);
+            }
+
+            int sum=0;
+
+
+            }
+
+        }
+    }
+
